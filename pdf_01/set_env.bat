c57339875ca8e8304bce8181dad273deb338702c
behai-nguyen <behai_nguyen@hotmail.com>
2025-11-28 00:12:55 +1100

    Fixed path to C:\PF\harfbuzz\dist\bin"

--- pdf_01/set_env.bat
+++ pdf_01/set_env.bat
@@ -1,7 +1,7 @@
 rem 26/10/2025
 
-rem C:\PF\harfbuzz\build\src\harfbuzz.dll
-set PATH=C:\PF\harfbuzz\build\src\;%PATH%
+rem C:\PF\harfbuzz\dist\bin\[harfbuzz.dll, harfbuzz-subset.dll, harfbuzz-gobject.dll]
+set PATH=C:\PF\harfbuzz\dist\bin\;%PATH%
 
 rem C:\PF\vcpkg\installed\x64-windows\bin\freetype.dll
 set PATH=C:\PF\vcpkg\installed\x64-windows\bin\;%PATH%
